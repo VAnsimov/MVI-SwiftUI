b6b9e03b2abc225e66c68c7fe17a516d55b11858
Viacheslav Ansimov <vansimove@gmail.com>
2022-12-04 10:34:01 +0600

    addwd MVContainer to xctemplate

---
+++ Templates_for_Xcode/xctemplate/SwiftUI MVI.xctemplate/MVIContainer.swift
@@ -0,0 +1,28 @@
+//___FILEHEADER___
+
+import SwiftUI
+import Combine
+
+final class MVIContainer<Intent, Model>: ObservableObject {
+
+    // MARK: Public
+
+    let intent: Intent
+    let model: Model
+
+    // MARK: private
+
+    private var cancellable: Set<AnyCancellable> = []
+
+    // MARK: Life cycle
+
+    init(intent: Intent, model: Model, modelChangePublisher: ObjectWillChangePublisher) {
+        self.intent = intent
+        self.model = model
+
+        modelChangePublisher
+            .receive(on: RunLoop.main)
+            .sink(receiveValue: objectWillChange.send)
+            .store(in: &cancellable)
+    }
+}
